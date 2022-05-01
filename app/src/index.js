af151482f39ae4ea7372914e67e63c616f58bb1c
Chris <chrisem816@gmail.com>
2022-05-01 08:43:48 -0700

    major changes, seems to be working

--- app/src/index.js
+++ app/src/index.js
@@ -62,7 +62,7 @@ const App = {
   },
 
   play: async function() {
-    const betAmount = document.getElementById("betAmount").value;
+    const betAmount = document.getElementById("wager").value;
     const leeway = document.getElementById("leeway").value;
     const guess = document.getElementById("guess").value;
 
@@ -107,3 +107,76 @@ window.addEventListener("load", function() {
   App.start();
 });
 export default App;
+
+$( document ).ready(function() {
+
+  $(document).on( 'click','#flexCheckChecked', async function () { 
+      $("#slider-row").toggle() 
+      
+      updateWinningNumbers()
+  });
+  
+  $(document).on( 'change','#guess', async function () { 
+      updateWinningNumbers()
+  });
+  
+  
+  $(document).on( 'change','#rangeValue1', async function () { 
+      updateWinningNumbers()
+  });
+  $(document).on( 'change','#wager', async function () { 
+      updateWinningNumbers()
+  });
+  
+  $(document).on( 'click','#slider-row', async function () { 
+     updateWinningNumbers()
+  });
+  $(document).on( 'click','#slider-row-1', async function () { 
+      updateWinningNumbers()
+   });
+  
+  
+  $(document).on( 'click','#send-button', async function () { 
+      //console.log(App.account)
+  
+   });
+  
+  
+  
+  
+  async function updateWinningNumbers(){
+      var guess = $("#guess").val();
+      var leeway = $("#leeway").val();
+      var numGuess = Number(guess);
+      var numLeeway = Number(leeway);
+      var min = numGuess - numLeeway;
+      var wager = Number($("#wager").val())
+      console.log(wager)
+      if(min<1){
+          min+=100;
+      }
+      var max = numGuess + numLeeway;
+      if(max>100){
+          max-=100;
+      }
+      $("#outcome").text("");
+      $("#number").text("");
+      if(leeway !== '0'){
+          var odds = ((100-(leeway*2+1))/(leeway*2+1)).toFixed(2);
+          var retOdds = (((100-(leeway*2+1))/(leeway*2+1)) - 0.05 * ((100-(leeway*2+1))/(leeway*2+1))).toFixed(2)
+      $("#win-con").show()
+      $("#num-range").text(`${min} - ${max}`)
+      $("#win-odds").text(`${leeway*2+1}% or ${odds}:1`);
+      $("#return-odds").text(`${retOdds}:1 [aka ${retOdds} coins profit for every 1 coin bet]`)
+      $("#return-win").text(`${Math.floor(retOdds * wager + wager)}`);
+  
+      } else{
+          $("#win-con").hide()
+          $("#num-range").text(`${numGuess}`)
+          $("#win-odds").text("1% or 99.00:1");
+          $("#return-odds").text(`80.00:1 [aka 80.00 coins profit for every 1 coin bet]`)
+          $("#return-win").text(`${Math.floor(80 * wager + wager)}`);
+      }
+  
+  }
+  });
